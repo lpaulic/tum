641966f7eef7fbcc33e01218a4a7f37f4b5a9f24
Luka Paulić <luka.paulic0@gmail.com>
2023-08-11 10:56:32 +0200

    feat: add resource monitoring
    Add mointoring crate
    Add display formating for MqttClientError
    Update error handing and module invocation in main

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,26 @@
-/*
-   TODO:
-       - rework this crate to be a binary crate
-       - run the code from here
-*/
-
+mod monitor;
 mod mqtt_client;
+mod resource;
 
-use gethostname::gethostname;
 use mqtt_client::MqttClient;
+use std::sync::Arc;
 
 fn main() {
     let username = "lpaulic";
     let password = "lpaulic";
+    let mqtt_client = MqttClient::new("localhost", 1883, username, password);
+    let resource_monitor = Arc::new(monitor::ResourceMonitor::new(1, mqtt_client));
 
-    let mut mqtt_client = MqttClient::new("localhost", 1883, username, password);
-    let topic = format!(
-        "device/{}/system/stats",
-        gethostname().into_string().unwrap()
-    );
-    if let Err(e) = mqtt_client.publish(&topic, "test message".as_bytes()) {
-        println!("Error {:?}", e);
+    // TODO: make OS specific signal handling, handle for Windows OS, currently unix systems are only supported
+    let sighandler_monitor = Arc::clone(&resource_monitor);
+    if let Err(e) = ctrlc::set_handler(move || match sighandler_monitor.stop() {
+        Ok(_) => std::process::exit(0),
+        Err(_) => std::process::exit(1),
+    }) {
+        println!("ERR: interrupt signal registration failed: {}", e)
+    }
+
+    if let Err(e) = resource_monitor.start() {
+        println!("ERR: resource monitor start issue: {}", e);
     }
 }
---
+++ src/monitor.rs
@@ -0,0 +1,129 @@
+use crate::mqtt_client::{MqttClient, MqttClientError};
+use crate::resource::Resource;
+use gethostname::gethostname;
+use std::any::Any;
+use std::boxed::Box;
+use std::fmt;
+use std::marker::Send;
+use std::sync::{Arc, Mutex};
+use std::thread;
+
+pub enum ResourceMonitorError {
+    ResourceSynchronization,
+    TransmissionSynchronization,
+    ExecutionSynchronization,
+    StringConversion,
+    Serialization,
+    Transmission(MqttClientError),
+}
+
+impl fmt::Display for ResourceMonitorError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match *self {
+            ResourceMonitorError::ResourceSynchronization => {
+                write!(f, "Synchronization issues for 'resource' attribute.")
+            }
+            ResourceMonitorError::TransmissionSynchronization => {
+                write!(f, "Synchronization issues for 'transmitter' attribute.")
+            }
+            ResourceMonitorError::ExecutionSynchronization => {
+                write!(f, "Synchronization issues for 'execution' attribute.")
+            }
+            ResourceMonitorError::StringConversion => {
+                write!(f, "Failed to convert data to string.")
+            }
+            ResourceMonitorError::Serialization => {
+                write!(f, "Failed to serialize resource data.")
+            }
+            ResourceMonitorError::Transmission(ref err) => write!(f, "MqttClient error: {}", err),
+        }
+    }
+}
+
+impl From<MqttClientError> for ResourceMonitorError {
+    fn from(item: MqttClientError) -> ResourceMonitorError {
+        ResourceMonitorError::Transmission(item)
+    }
+}
+
+impl From<Box<dyn Any + Send>> for ResourceMonitorError {
+    fn from(item: Box<dyn Any + Send>) -> ResourceMonitorError {
+        item.into()
+    }
+}
+
+pub struct ResourceMonitor {
+    sampling_rate_s: u64,
+    resource: Arc<Mutex<Resource>>,
+    transmitter: Arc<Mutex<MqttClient>>,
+    stop_monitoring: Arc<Mutex<bool>>,
+}
+
+impl ResourceMonitor {
+    pub fn new(sampling_rate_s: u64, transmitter: MqttClient) -> ResourceMonitor {
+        ResourceMonitor {
+            sampling_rate_s: if sampling_rate_s > 0 {
+                sampling_rate_s
+            } else {
+                5
+            },
+            resource: Arc::new(Mutex::new(Resource::new())),
+            transmitter: Arc::new(Mutex::new(transmitter)),
+            stop_monitoring: Arc::new(Mutex::new(false)),
+        }
+    }
+
+    pub fn start(&self) -> Result<(), ResourceMonitorError> {
+        let resource = Arc::clone(&self.resource);
+        let transmitter = Arc::clone(&self.transmitter);
+        let stop_monitoring = Arc::clone(&self.stop_monitoring);
+        let sampling_rate_s = self.sampling_rate_s;
+
+        let handler = thread::spawn(move || -> Result<(), ResourceMonitorError> {
+            loop {
+                match resource.lock() {
+                    Ok(mut res) => res.refresh(),
+                    Err(_) => return Err(ResourceMonitorError::ResourceSynchronization),
+                }
+
+                let topic = match gethostname().to_str() {
+                    Some(hostname) => format!("device/{}/system/stats", hostname),
+                    None => return Err(ResourceMonitorError::StringConversion),
+                };
+
+                let data = match serde_json::to_vec(resource.as_ref()) {
+                    Ok(data) => data,
+                    Err(_) => return Err(ResourceMonitorError::Serialization),
+                };
+
+                match transmitter.lock() {
+                    Ok(mut tx) => tx.publish(&topic, data.as_slice())?,
+                    Err(_) => return Err(ResourceMonitorError::TransmissionSynchronization),
+                };
+
+                match stop_monitoring.lock() {
+                    Ok(stop_monitoring) => {
+                        if *stop_monitoring {
+                            break;
+                        }
+                    }
+                    Err(_) => return Err(ResourceMonitorError::ExecutionSynchronization),
+                }
+
+                thread::sleep(std::time::Duration::from_secs(sampling_rate_s));
+            }
+            Ok(())
+        });
+
+        handler.join()?
+    }
+
+    pub fn stop(&self) -> Result<(), ResourceMonitorError> {
+        match self.stop_monitoring.lock() {
+            Ok(mut stop_monitoring) => *stop_monitoring = true,
+            Err(_) => return Err(ResourceMonitorError::ExecutionSynchronization),
+        }
+
+        Ok(())
+    }
+}
